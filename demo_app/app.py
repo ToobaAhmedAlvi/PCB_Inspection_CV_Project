53326a0c5507ebd29160813efdfc3a1e5a953a9d
Tooba Ahmed Alvi <alvi.tooba@gmail.com>
2025-11-26 21:00:14 +0500

    updated image paths for sample PCB buttons

--- demo_app/app.py
+++ demo_app/app.py
@@ -199,11 +199,11 @@ def main():
     
     sample_clicked = None
     if col1.button("Sample PCB 1"):
-        sample_clicked = "Test_images/image1.jpg"
+        sample_clicked = "Test_images/image4.jpg"
     if col2.button("Sample PCB 2"):
-        sample_clicked = "Test_images/image2.jpg"
+        sample_clicked = "Test_images/image2.jpeg"
     if col3.button("Sample PCB 3"):
-        sample_clicked = "Test_images/image3.jpg"
+        sample_clicked = "Test_images/image5.jpg"
     
     # Load image
     image = None
